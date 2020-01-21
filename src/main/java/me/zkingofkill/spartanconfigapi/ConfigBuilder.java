2a2f68b413391a7afa32be026bb1a4f4f944d337
pedro <pedroeugeniocavalcanti@gmail.com>
2020-01-20 22:28:36 -0300

    corrections and additions

--- src/main/java/me/zkingofkill/spartanconfigapi/ConfigBuilder.java
+++ src/main/java/me/zkingofkill/spartanconfigapi/ConfigBuilder.java
@@ -2,6 +2,10 @@ package me.zkingofkill.spartanconfigapi;
 
 import me.zkingofkill.spartanconfigapi.annotations.Config;
 import me.zkingofkill.spartanconfigapi.annotations.ConfigValue;
+import me.zkingofkill.spartanconfigapi.exceptions.IncompatibleTypeException;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
@@ -9,6 +13,10 @@ import org.bukkit.plugin.Plugin;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class ConfigBuilder {
     /**
@@ -16,60 +24,115 @@ public abstract class ConfigBuilder {
      * @version 1.0
      * @category Utils
      */
-    public void init(Plugin plugin, Object object) {
-        Class clazz = super.getClass();
-        if (clazz.isAnnotationPresent(Config.class)) {
-            Field[] fields = clazz.getDeclaredFields();
 
+    private Object object;
+    private File file;
+    private Class clazz;
+    public FileConfiguration fileConfiguration;
+
+    public void init(Plugin plugin, Object object) throws IncompatibleTypeException {
+        this.object = object;
+        clazz = super.getClass();
+        if (clazz.isAnnotationPresent(Config.class)) {
             String fileName = ((Config) clazz.getAnnotation(Config.class)).path();
-            File file = new File(plugin.getDataFolder(), fileName);
-            FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
-            if (!file.exists()) {
+            file = new File(plugin.getDataFolder(), fileName);
+            fileConfiguration = YamlConfiguration.loadConfiguration(file);
+            saveConfig();
+        } else throw new IncompatibleTypeException();
+    }
+
+    void saveConfig() {
+        List<Field> fields = getFieldList(clazz);
+        if (!file.exists()) {
+            try {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        updateFields(object, file, fileConfiguration, fields);
+    }
+
+    public void reloadConfig() {
+        fileConfiguration = YamlConfiguration.loadConfiguration(file);
+    }
+
+    private List<Field> getFieldList(Class clazz) {
+        List<Field> fields = new ArrayList(Arrays.asList(clazz.getDeclaredFields()));
+        fields.addAll(Arrays.asList(clazz.getSuperclass().getDeclaredFields()));
+        return fields;
+    }
+
+    <T> T parse(Class<T> clazz, File file) throws IncompatibleTypeException, InstantiationException, IllegalAccessException {
+        T object = clazz.newInstance();
+        if (clazz.isAnnotationPresent(Config.class)) {
+            List<Field> fields = getFieldList(clazz);
+            YamlConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
+            updateFields(object, file, fileConfiguration, fields);
+            return ((T) object);
+        }
+        throw new IncompatibleTypeException();
+    }
+
+    private void updateFields(Object object, File file, FileConfiguration fileConfiguration, List<Field> fields) {
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(ConfigValue.class)) {
+                if (!field.isAccessible()) {
+                    field.setAccessible(true);
+                }
+
+                String fieldpath = field.getAnnotation(ConfigValue.class).path();
+                if (!fieldpath.equals("")) {
+                    fieldpath = field.getName();
+                }
+                Object fieldvalue = null;
+                boolean translateAlternativeColors = field.getAnnotation(ConfigValue.class).translateAlternativeColors();
                 try {
-                    file.getParentFile().mkdirs();
-                    file.createNewFile();
-                } catch (IOException e) {
+                    fieldvalue = field.get(object);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                if (!fileConfiguration.contains(fieldpath)) {
+                    fileConfiguration.set(fieldpath, fieldvalue);
+                    try {
+                        fileConfiguration.save(file);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    Object value = fileConfiguration.get(fieldpath);
+                    if (translateAlternativeColors) {
+                        if (value instanceof String) {
+                            value = ChatColor.translateAlternateColorCodes('&', (String) value);
+                        }
+                        if (value instanceof List) {
+                            value = ((List<String>) value).stream().map(o -> o.contains("&") ? ChatColor.translateAlternateColorCodes('&', o) : o).collect(Collectors.toList());
+                        }
+
+                    }
+                    field.set(object, value);
+                } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
             }
-            for (Field field : fields) {
-                if (field.isAnnotationPresent(ConfigValue.class)) {
-                    if (!field.isAccessible()) {
-                        field.setAccessible(true);
-                    }
-                    String fieldpath = field.getAnnotation(ConfigValue.class).path();
-                    Object fieldvalue = null;
-                    boolean translateAlternativeColors = field.getAnnotation(ConfigValue.class).translateAlternativeColors();
-                    try {
-                        fieldvalue = field.get(object);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                    if (!fileConfiguration.contains(fieldpath)) {
-                        fileConfiguration.set(fieldpath, fieldvalue);
-                        try {
-                            fileConfiguration.save(file);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    try {
-                        Object value = fileConfiguration.get(fieldpath);
-                        if (translateAlternativeColors) {
-                            if (value instanceof String) {
-                                value = ((String) fieldvalue).replace("&", "§");
-                            }
-
-                        }
-                        field.set(object, value);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            }
-
         }
+    }
 
+    public void addVariable(List<String> list, String var, Object replacement) {
+        list.stream().map(o -> o.contains(var) ? o.replace(var, replacement.toString()) : o).collect(Collectors.toList());
+    }
+
+    public void sendAll(List<String> msg) {
+        Bukkit.getOnlinePlayers().forEach(player -> msg.forEach(m -> player.sendMessage(m)));
+    }
+
+    public void send(CommandSender sd, List<String> msg) {
+        msg.forEach(sd::sendMessage);
+    }
+
+    public void sendAll(String msg) {
+        Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage(msg));
     }
 }
---
+++ src/main/java/me/zkingofkill/spartanconfigapi/exceptions/IncompatibleTypeException.java
@@ -0,0 +1,7 @@
+package me.zkingofkill.spartanconfigapi.exceptions;
+
+public class IncompatibleTypeException extends Exception {
+    public IncompatibleTypeException() {
+        super();
+    }
+}
